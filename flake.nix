1bf504a36be8691fd6cfc13547cb9978695a99b8
mekter <itsuout.tcbn@gmail.com>
2022-08-13 06:33:15 +0700

    flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    
+  };
+}
