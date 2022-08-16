d0f1cbacc745ec456d6874cf277230622c49331c
mekter <itsuout.tcbn@gmail.com>
2022-08-17 01:57:58 +0700

    change fish theme to nord

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -7,39 +7,45 @@
       set HOMECONF $HOME/nixos-config/home
       set ROOTCONF $HOME/nixos-config/root
 
-      # TokyoNight Color Palette
-      set -l foreground c0caf5
-      set -l selection 33467C
-      set -l comment 565f89
-      set -l red f7768e
-      set -l orange ff9e64
-      set -l yellow e0af68
-      set -l green 9ece6a
-      set -l purple 9d7cd8
-      set -l cyan 7dcfff
-      set -l pink bb9af7
+      # Colorscheme: Nord
+      set -U fish_color_normal normal
+      set -U fish_color_command 81a1c1
+      set -U fish_color_quote a3be8c
+      set -U fish_color_redirection b48ead
+      set -U fish_color_end 88c0d0
+      set -U fish_color_error ebcb8b
+      set -U fish_color_param eceff4
+      set -U fish_color_comment 434c5e
+      set -U fish_color_match --background=brblue
+      set -U fish_color_selection white --bold --background=brblack
+      set -U fish_color_search_match bryellow --background=brblack
+      set -U fish_color_history_current --bold
+      set -U fish_color_operator 00a6b2
+      set -U fish_color_escape 00a6b2
+      set -U fish_color_cwd green
+      set -U fish_color_cwd_root red
+      set -U fish_color_valid_path --underline
+      set -U fish_color_autosuggestion 4c566a
+      set -U fish_color_user brgreen
+      set -U fish_color_host normal
+      set -U fish_color_cancel --reverse
+      set -U fish_pager_color_prefix normal --bold --underline
+      set -U fish_pager_color_progress brwhite --background=cyan
+      set -U fish_pager_color_completion normal
+      set -U fish_pager_color_description B3A06D
+      set -U fish_pager_color_selected_background --background=brblack
+      set -U fish_pager_color_selected_completion
+      set -U fish_pager_color_secondary_background
+      set -U fish_pager_color_secondary_description
+      set -U fish_color_option
+      set -U fish_pager_color_secondary_completion
+      set -U fish_color_host_remote
+      set -U fish_pager_color_background
+      set -U fish_pager_color_selected_prefix
+      set -U fish_color_keyword
+      set -U fish_pager_color_secondary_prefix
+      set -U fish_pager_color_selected_description
 
-      # Syntax Highlighting Colors
-      set -g fish_color_normal $foreground
-      set -g fish_color_command $cyan
-      set -g fish_color_keyword $pink
-      set -g fish_color_quote $yellow
-      set -g fish_color_redirection $foreground
-      set -g fish_color_end $orange
-      set -g fish_color_error $red
-      set -g fish_color_param $purple
-      set -g fish_color_comment $comment
-      set -g fish_color_selection --background=$selection
-      set -g fish_color_search_match --background=$selection
-      set -g fish_color_operator $green
-      set -g fish_color_escape $pink
-      set -g fish_color_autosuggestion $comment
-
-      # Completion Pager Colors
-      set -g fish_pager_color_progress $comment
-      set -g fish_pager_color_prefix $cyan
-      set -g fish_pager_color_completion $foreground
-      set -g fish_pager_color_description $comment
     '';
     functions = {
       fish_greeting = "";
