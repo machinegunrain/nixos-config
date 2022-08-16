cde8ec3721bf0b4b4dbcd83f35a8c96f7572cfef
mekter <itsuout.tcbn@gmail.com>
2022-08-16 17:28:40 +0700

    hah?

--- home/spotify/default.nix
+++ home/spotify/default.nix
@@ -2,7 +2,7 @@
 
 {
 
-  home.packages = with pkgs; [spotify spicetify-cli];
+  home.packages = with pkgs; [spotify-unwrapped spicetify-cli];
   home.file."config/spicetify/Themes".source = ../conf.d/spicetify/Themes;
   # xdg.configFile."spicetify".source = ../conf.d/spicetify;
 }
