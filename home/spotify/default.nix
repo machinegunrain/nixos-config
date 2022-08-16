a3d258eada22dba61dee758fc94240995053d700
mekter <itsuout.tcbn@gmail.com>
2022-08-16 17:13:28 +0700

    create theme link

--- home/spotify/default.nix
+++ home/spotify/default.nix
@@ -3,5 +3,6 @@
 {
 
   home.packages = with pkgs; [spotify spicetify-cli];
-  xdg.configFile."spicetify".source = ../conf.d/spicetify;
+  home.file."config/spicetify/Themes".source = ../conf.d/spicetify/Themes;
+  # xdg.configFile."spicetify".source = ../conf.d/spicetify;
 }
