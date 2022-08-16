667dc9673e41fa619a840afa0390570246a67c96
mekter <itsuout.tcbn@gmail.com>
2022-08-16 16:22:41 +0700

    fix gtx

--- home/gtk/default.nix
+++ home/gtk/default.nix
@@ -2,22 +2,23 @@
 let
   configure-gtk = pkgs.writeTextFile {
   name = "configure-gtk";
-  destination = "/bin/configue-gtk";
+  destination = "/bin/configure-gtk";
   executable = true;
   text = let
   schema = pkgs.gsettings-desktop-schemas;
   datadir = "${schema}/share/gsetting-schemas/${schema.name}";
   in ''
       export XDG_DATA_DIR=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnom.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'Dracula';
+      gnome_schema=org.gnome.desktop.interface
+      gsettings set $gnome_schema gtk-theme 'Dracula'
+      gsettings set $gnome_schema icon-theme 'Adwaita'
   '';
   };
 
 in
 {
   home.packages = with pkgs; [
-    configure-gtk gsettings
+    configure-gtk glib
     dracula-theme gnome3.adwaita-icon-theme
   ];
 }
