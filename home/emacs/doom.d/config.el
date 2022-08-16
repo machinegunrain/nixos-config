5878e22de3bd0e7097711697a9f11db5d37b8247
mekter <itsuout.tcbn@gmail.com>
2022-08-17 03:59:39 +0700

    change neotree color

--- home/emacs/doom.d/config.el
+++ home/emacs/doom.d/config.el
@@ -2,6 +2,6 @@
       doom-font (font-spec :family "PragmataPro Mono Liga" :size 18 :weight 'regular))
 
 (custom-set-faces
- '(neo-dir-link-face ((t (:foreground "#7dcfff" :slant normal :weight  normal :height 120 :family "Iosevka"))))
- '(neo-file-link-face ((t (:foreground "#c0caf5" :weight normal :height 120 :family "Iosevka"))))
- '(neo-root-dir-face ((t (:foreground "#7dcfff" :weight  normal)))))
+ '(neo-dir-link-face ((t (:slant normal :weight  normal :height 120 :family "Iosevka"))))
+ '(neo-file-link-face ((t (:weight normal :height 120 :family "Iosevka"))))
+ '(neo-root-dir-face ((t (:weight  normal)))))
