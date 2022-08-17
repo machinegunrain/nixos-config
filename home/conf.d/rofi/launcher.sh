e7296526a7bc7d8a1ba0d05ad8a0e0255e6f6ae7
mekter <itsuout.tcbn@gmail.com>
2022-08-18 04:04:43 +0700

    config rofi theme

--- home/conf.d/rofi/launcher.sh
+++ home/conf.d/rofi/launcher.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
+## Author  : Manita Talcharus
+## Mail    : manita.tcrs@pm.me
+## Github  : @mekter
+## Mastodon : @dash@mastodon.in.th
 
-# Available Styles
-# >> Created and tested on : rofi 1.6.0-1
-#
-# style_1     style_2     style_3     style_4     style_5     style_6     style_7
 
-theme="style_city-light.rasi"
+theme="style_nord-light.rasi"
 
 dir="$HOME/.config/rofi/launchers/text"
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
 color="${styles[$(( $RANDOM % 10 ))]}"
 
-# comment this line to disable random colors
-# sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
-
-# comment these lines to disable random style
-# themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
-# theme="${themes[$(( $RANDOM % 7 ))]}"
-
 rofi -no-lazy-grab -show run \
--modi run,drun,window \
+-modi run,drun \
 -theme $dir/"$theme"
-
-
-
