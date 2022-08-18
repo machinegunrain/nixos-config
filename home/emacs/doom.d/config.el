a538265db685ad4ea7d5b98eb3a0da431a784b03
mekter <itsuout.tcbn@gmail.com>
2022-08-18 19:49:25 +0700

    change to treemacs and trying treesitter

--- home/emacs/doom.d/config.el
+++ home/emacs/doom.d/config.el
@@ -4,3 +4,5 @@
       doom-font (font-spec :family "PragmataPro Mono Liga" :size 18 :weight 'regular)
       doom-variable-pitch-font (font-spec :family "Iosevka")
       doom-big-font (font-spec :family "PragmataPro Mono Liga" :size 22))
+
+(setq +tree-sitter-hl-enabled-modes t)
