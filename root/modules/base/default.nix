0e59acdfafae0586c73a910f0acaf0a1394721cf
mekter <itsuout.tcbn@gmail.com>
2022-09-08 18:40:38 +0700

    remove glibc from root base packages

--- root/modules/base/default.nix
+++ root/modules/base/default.nix
@@ -16,7 +16,7 @@
 
   # root packages
   environment.systemPackages = with pkgs; [
-    wget parted git ntfs3g cachix libglvnd gnumake gcc glibc
+    wget parted git ntfs3g cachix libglvnd gnumake gcc
     libde265 openh264 faad2 xvidcore ffmpeg_5 libmad
     pavucontrol nerdfonts zlib sqlite
     freeglut ghostscript inotify-tools
